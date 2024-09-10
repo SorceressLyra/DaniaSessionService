cc00ad25d10243f2fc7860f26072d3ae4775fc80
Lyra <lyramikkelsen@gmail.com>
2024-09-10 12:30:00 +0200

    Add project files.

---
+++ Controllers/SessionsController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace DaniaSessionService
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SessionsController : ControllerBase
+    {
+
+        [HttpGet("Verify")]
+        public IActionResult AuthorizationTest(string endpoint)
+        {
+            if (!SessionStorage.Sessions.ContainsValue(endpoint))
+                return NotFound($"No user session found for ip:{endpoint}");
+
+            return Ok($"{SessionStorage.Sessions.FirstOrDefault(x => x.Value == endpoint).Key} on ip:{endpoint} is logged in");
+        }
+
+
+        [HttpPost("Add")    ]
+        public IActionResult AddSession(string username, string endpoint)
+        {
+            if (SessionStorage.Sessions.ContainsKey(username))
+                return BadRequest($"Session already created for {username}");
+            ;
+            SessionStorage.Add(username, endpoint);
+            return Ok($"Session created for {username} at ip:{endpoint}");
+        }
+
+        [HttpPost("Remove")]
+        public IActionResult RemoveSession(string username)
+        {
+            if (!SessionStorage.Sessions.ContainsKey(username))
+                return NotFound($"No sessions found for user: {username}");
+
+
+            SessionStorage.Remove(username);
+            return Ok($"Removed session from user: {username}");
+        }
+
+
+        [HttpGet("All")]
+        public IActionResult GetSessions()
+        {
+            return Ok(SessionStorage.Sessions);
+        }
+    }
+}
---
+++ SessionStorage.cs
@@ -0,0 +1,19 @@
+﻿namespace DaniaSessionService
+{
+    public static class SessionStorage
+    {
+        private static Dictionary<string, string> sessions = new();
+
+        public static Dictionary<string, string> Sessions => sessions;
+
+        public static void Add(string key, string value)
+        {
+            Sessions.Add(key, value);
+        }
+
+        public static void Remove(string key)
+        {
+            sessions.Remove(key);
+        }
+    }
+}
